ffbd13a2fc58e41750e2c658044c82c2ebf637e9
XantreGodlike <valerik007.vs@gmail.com>
2023-12-25 18:58:22 +0100

    some fixes

--- App.js
+++ App.js
@@ -1,21 +1,4 @@
-// import "./global.css";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+import { App } from "./src/App";
 
-export default function App() {
-  return (
-    <View className="flex-1 bg-black items-center justify-center">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
