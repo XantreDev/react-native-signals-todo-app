c377af55095ae1807f14b90f275aea40cee0dfe1
XantreGodlike <valerik007.vs@gmail.com>
2023-12-30 17:37:32 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "../global.css";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
+// Ready to implement TODO app
+
 export const App = () => (
   <View className="flex-1 items-center justify-center">
     <Text>Open up App.js to start working on your app!</Text>
