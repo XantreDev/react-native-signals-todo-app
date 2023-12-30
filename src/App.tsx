45d265610ff30b17d01eb194117e4dac539e4c4e
XantreGodlike <valerik007.vs@gmail.com>
2023-12-30 18:10:17 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import "../global.css";
 
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, TextInput, View } from "react-native";
 
-// Ready to implement TODO app
+const CrazyComponent = () => {
+  let a = 0;
+  for (let i = 0; i < 50_000_000; i++) {
+    a += i;
+    a |= 0;
+  }
+  return <Text>{a}</Text>;
+};
 
-export const App = () => (
-  <View className="flex-1 items-center justify-center">
-    <Text>Open up App.js to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
+const Input = ({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+}) => (
+  <TextInput
+    className="border self-stretch"
+    value={value}
+    onChangeText={onChange}
+  />
 );
+
+export const App = () => {
+  const [state, setState] = React.useState("");
+
+  return (
+    <View className="flex-1 items-center justify-center px-4">
+      <Text className="text-2xl mb-2">Here is our input</Text>
+      <CrazyComponent />
+      <Input value={state} onChange={setState} />
+      <Text className="text-3xl">Here is your text: {state}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
